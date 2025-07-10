5c25ac02a910817573cb6f0bd74520d25d3808a7
Tonći Vatavuk <tonci@sistemi.hr>
2025-07-10 11:43:53 +0200

    Update `*.nuspec`, `*.nuspectemplate` files handling in scripts

--- Package/debug.cmd
+++ Package/debug.cmd
@@ -2,4 +2,7 @@
 set TargetFramework=%1
 set ProjectName=%2
 
-XCOPY "..\Client\wwwroot\*" "..\..\oqtane.framework\Oqtane.Server\wwwroot\" /Y /S /I
+rem workaround, part 2/2, to ensure that *.nuspectemplate files are renamed back to *.nuspec
+for /r "..\Client\wwwroot\Themes\Templates" %%f in (*.nuspectemplate) do ren "%%f" "%%~nf.nuspec"
+
+xcopy "..\Client\wwwroot\*" "..\..\oqtane.framework\Oqtane.Server\wwwroot\" /Y /S /I
